31e468f859161696c513a8c792466d588a5f8b4a
Konstantin Nedosekin <knedosekin@gmail.com>
2019-11-12 12:28:33 +0300

    Задание 2, додумался перевести в кортеж и потом определить длину. Где то мы уже это делали)

--- les-5.py
+++ les-5.py
@@ -1,1 +1,17 @@
 # домашнеезадание №1
+f = open(r"text_1.txt", "a")
+while True:
+    my_text = input("Введите текст для записи в файл - ")
+    f.write(my_text + '\n')
+    if not my_text:
+        break
+f.close()
+
+# домашнеезадание №2
+with open("text_2.txt") as f:
+    my_line = f.readlines()
+    for index, value in enumerate(my_line):
+        number_of_words = len(value.split())
+        print('Строка {} содержит {} слов'.format(index + 1, number_of_words))
+
+# домашнеезадание №3
